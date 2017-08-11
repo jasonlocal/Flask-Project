1a43745315588271335bdf8c4064afca98b7f6db
Jason Zheng <jasonlocation@gmail.com>
2017-08-10 23:35:03 -0400

    updated config file

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os 
 basedir= os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI ='postgresql://' + os.path.join(basedir,'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/userlogin'#'postgresql:///' + os.path.join(basedir,'app.db')
+SQLALCHEMY_TRACK_MODIFICATIONS= True
