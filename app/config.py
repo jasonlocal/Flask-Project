d0398e54e4cb3cce5c918f4c5f45fed8f3762004
Jason Zheng <jasonlocation@gmail.com>
2017-08-26 13:50:25 -0400

    deplicated config files in wrong place

--- app/config.py
+++
@@ -1,4 +0,0 @@
-import os 
-basedir= os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/userlogin'#'postgresql:///' + os.path.join(basedir,'app.db')
-SQLALCHEMY_TRACK_MODIFICATIONS= True
